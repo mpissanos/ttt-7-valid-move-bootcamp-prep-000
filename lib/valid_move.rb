1b2fb3013a7a31aa9ba226b576042f45759b14ed
Manuel Pissanos <mpissano@gmail.com>
2018-08-29 20:28:14 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,12 +5,12 @@
 
 
 def valid_move?(board, index)
-if index.between?(0, 8)  && !position_taken?
+if index.between?(0, 8)  && position_taken?
  end
 end
 
 
 def position_taken?(board, index)
- ! (board[index] == " " || board[index] == "" || board[index] == nil)
+ (board[index] == " " || board[index] == "" || board[index] == nil)
 end
 
